bcf9b7343590823ba559bd57de798c439049507e
Aahan Salecha <salecha.aahan@gmail.com>
2025-02-10 16:31:54 +0530

    Update messaging.py
    Add password protection

--- source/messaging.py
+++ source/messaging.py
@@ -13,14 +13,19 @@ SQL = Connection(DIRECTORY+"\\database.db")
 SQL_CURSOR = SQL.cursor()
 SQL_CURSOR.execute("CREATE TABLE IF NOT EXISTS Messages (timestamp TEXT, from_ TEXT, message TEXT, to_ TEXT)")
 
-def send(from_: str, message: str, to: str) -> None:
+def send(from_: str, message: str, to: str, fromPassword: str) -> None:
+    if fromPassword != decrypt(SQL_CURSOR.execute("SELECT * FROM Users WHERE username = ?", (from_, )).fetchone()[2], GLOBAL_KEY):
+        return
     seed = SQL_CURSOR.execute("SELECT * FROM Users WHERE username = ?", (to, )).fetchone()[3]
     SQL_CURSOR.execute("INSERT INTO Messages (timestamp, from_, message, to_) VALUES (?, ?, ?, ?)", (f'{datetime.now().date()} {datetime.now().hour}:{datetime.now().minute}.{datetime.now().second}', from_, encrypt(message, Key(seed=int(seed))), to))
     SQL.commit()
 
-def get_messages(chat: str) -> list[str]:
+def get_messages(chat: str, password: str) -> list[str]:
     user1 = chat.split("-")[0]
     user2 = chat.split("-")[1]
+
+    if password != decrypt(SQL_CURSOR.execute("SELECT * FROM Users WHERE username = ?", (user1, )).fetchone()[2], GLOBAL_KEY) and password != decrypt(SQL_CURSOR.execute("SELECT * FROM Users WHERE username = ?", (user2, )).fetchone()[2], GLOBAL_KEY):
+        return []
 
     query = """
     SELECT * FROM Messages
